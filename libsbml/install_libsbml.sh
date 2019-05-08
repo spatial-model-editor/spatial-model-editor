414da41837554aa6f364d87ef9c77c06dddddc3b
lkeegan <liam@keegan.ch>
2019-05-08 17:40:56 +0200

    app

--- libsbml/install_libsbml.sh
+++ libsbml/install_libsbml.sh
@@ -1,9 +1,3 @@
-# script to extract win64 static library from sourceforge
-wget https://sourceforge.net/projects/sbml/files/libsbml/5.18.0/experimental/binaries/Windows/libSBML-5.18.0-win64.zip
-unzip libSBML-5.18.0-win64.zip
-mv libSBML-5.18.0-win64/lib/libsbml-static.lib .
-rm -rf libSBML-5.18.0-win64
-
 # script to download & compile libSBML experimental branch for linux, including spatial extension
 # creates `sbml` dir containing header files and `libsbml-static.a` library in the current directory
 svn co https://svn.code.sf.net/p/sbml/code/branches/libsbml-experimental tmp
