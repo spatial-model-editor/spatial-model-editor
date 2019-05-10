d150f5f1cfa644a38e4d9706f7b1838246c58340
lkeegan <liam@keegan.ch>
2019-05-10 13:44:12 +0200

    removed libsbml includes from git repo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QString>
+#include <sbml/SBMLTypes.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,11 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_action_About_triggered()
+{
+    QMessageBox msgBox;
+    msgBox.setText("libSBML version: " + QString(libsbml::getLibSBMLDottedVersion()));
+    msgBox.exec();
 }
