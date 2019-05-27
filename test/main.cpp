26c587860fabeb60d4772233a5d0db2f6693438e
Liam Keegan <liam@keegan.ch>
2019-05-27 10:17:40 +0200

    appveyor fix: use catch config runner to insert QCoreApplication for windows build

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,14 @@
-#define CATCH_CONFIG_MAIN
-
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+#include <QCoreApplication>
+//#include <QtTest>
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    int result = Catch::Session().run(argc, argv);
+
+    return result;
+}
