ff2287c8fe5bba90dba51f247bc21c4d0d4c99af
Liam Keegan <liam@keegan.ch>
2019-05-27 16:12:36 +0200

    travis linux/osx deployment fix

--- spatial-model-editor.pro
+++ spatial-model-editor.pro
@@ -43,3 +43,6 @@ win32: QMAKE_CXXFLAGS += -Wa,-mbig-obj
 
 # on windows statically link to avoid missing dll errors:
 win32: QMAKE_LFLAGS += -static-libgcc -static-libstdc++
+
+# on osx: set visibility to match setting used for compiling static libs
+mac: QMAKE_CXXFLAGS += -fvisibility=default
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -161,21 +161,25 @@ void MainWindow::on_listReactions_currentTextChanged(const QString &currentText)
 void MainWindow::on_btnSimulate_clicked()
 {
     // do simple simulation of model
+    // compile reaction expressions
     simulate sim(sbml_doc);
     sim.compile_reactions();
+    // set initial concentrations
     for (unsigned int i=0; i<sbml_doc.model->getNumSpecies(); ++i){
         sim.species_values[i] = sbml_doc.model->getSpecies(i)->getInitialConcentration();
     }
+    // generate vector of resulting concentrations at each timestep
     sim.euler_timestep(0.0);
     QVector<double> time;
     double t=0;
-    double dt=0.1;
+    double dt=ui->txtSimDt->text().toDouble();
     std::vector<QVector<double>> conc(sim.species_values.size());
     time.push_back(t);
     for (std::size_t i=0; i<sim.species_values.size(); ++i){
         conc[i].push_back(sim.species_values[i]);
     }
-    for(int i=0; i<500; ++i){
+    for(int i=0; i<static_cast<int>(ui->txtSimLength->text().toDouble()/dt); ++i){
+        // do an euler integration timestep
         sim.euler_timestep(dt);
         t += dt;
         time.push_back(t);
@@ -183,8 +187,8 @@ void MainWindow::on_btnSimulate_clicked()
             conc[i].push_back(sim.species_values[i]);
         }
     }
-
     // plot results
+    ui->pltPlot->clearGraphs();
     std::vector<QColor> col {{0, 0, 0}, {230, 25, 75}, {60, 180, 75}, {255, 225, 25}, {0, 130, 200}, {245, 130, 48}, {145, 30, 180}, {70, 240, 240}, {240, 50, 230}, {210, 245, 60}, {250, 190, 190}, {0, 128, 128}, {230, 190, 255}, {170, 110, 40}, {255, 250, 200}, {128, 0, 0}, {170, 255, 195}, {128, 128, 0}, {255, 215, 180}, {0, 0, 128}, {128, 128, 128}};
     ui->pltPlot->legend->setVisible(true);
     for (std::size_t i=0; i<sim.species_values.size(); ++i){
