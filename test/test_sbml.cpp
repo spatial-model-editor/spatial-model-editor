fe74776ac3ec0a63256908da9d15ca52006611fb
Liam Keegan <liam@keegan.ch>
2019-06-05 10:45:32 +0200

    add sbml compartment colour tests, some refactoring

--- test/test_sbml.cpp
+++ test/test_sbml.cpp
@@ -3,11 +3,11 @@
 
 #include "sbml.h"
 
-TEST_CASE("load SBML level 2 document") {
+SCENARIO("import SBML level 2 document", "[sbml]") {
   // create simple SBML level 2.4 model
   std::unique_ptr<libsbml::SBMLDocument> document(
       new libsbml::SBMLDocument(2, 4));
-  // crate model
+  // create model
   auto *model = document->createModel();
   // create two compartments of different size
   for (int i = 0; i < 2; ++i) {
@@ -37,14 +37,18 @@ TEST_CASE("load SBML level 2 document") {
   auto *kin = model->createKineticLaw();
   kin->setFormula("5*spec0c0");
   reac->setKineticLaw(kin);
+  // create a function
+  auto *func = model->createFunctionDefinition();
+  func->setId("func1");
   // write SBML document to file
   libsbml::SBMLWriter w;
   w.writeSBML(document.get(), "tmp.xml");
 
+  sbmlDocWrapper s;
+  s.importSBMLFile("tmp.xml");
+
   GIVEN("SBML document") {
-    WHEN("loadFile called") {
-      sbmlDocWrapper s;
-      s.loadFile("tmp.xml");
+    WHEN("importSBMLFile called") {
       THEN("find compartments") {
         REQUIRE(s.compartments.size() == 2);
         REQUIRE(s.compartments[0] == "compartment0");
@@ -60,15 +64,53 @@ TEST_CASE("load SBML level 2 document") {
         REQUIRE(s.species["compartment1"][1] == "spec1c1");
         REQUIRE(s.species["compartment1"][2] == "spec2c1");
       }
-      THEN("find species") {
-        REQUIRE(s.species.size() == 2);
-        REQUIRE(s.species["compartment0"].size() == 2);
-        REQUIRE(s.species["compartment0"][0] == "spec0c0");
-        REQUIRE(s.species["compartment0"][1] == "spec1c0");
-        REQUIRE(s.species["compartment1"].size() == 3);
-        REQUIRE(s.species["compartment1"][0] == "spec0c1");
-        REQUIRE(s.species["compartment1"][1] == "spec1c1");
-        REQUIRE(s.species["compartment1"][2] == "spec2c1");
+      THEN("find reactions") {
+        REQUIRE(s.reactions.size() == 1);
+        REQUIRE(s.reactions[0] == "reac1");
+      }
+      THEN("find functions") {
+        REQUIRE(s.functions.size() == 1);
+        REQUIRE(s.functions[0] == "func1");
+      }
+    }
+  }
+  GIVEN("Compartment Colours") {
+    QRgb col1 = 34573423;
+    QRgb col2 = 1334573423;
+    QRgb col3 = 17423;
+    WHEN("compartment colours have been assigned") {
+      s.setCompartmentColour("compartment0", col1);
+      s.setCompartmentColour("compartment1", col2);
+      THEN("can get CompartmentID from colour") {
+        REQUIRE(s.getCompartmentID(col1) == "compartment0");
+        REQUIRE(s.getCompartmentID(col2) == "compartment1");
+      }
+      THEN("can get colour from CompartmentID") {
+        REQUIRE(s.getCompartmentColour("compartment0") == col1);
+        REQUIRE(s.getCompartmentColour("compartment1") == col2);
+      }
+    }
+    WHEN("new colour assigned") {
+      s.setCompartmentColour("compartment0", col1);
+      s.setCompartmentColour("compartment1", col2);
+      s.setCompartmentColour("compartment0", col3);
+      THEN("unassign old colour mapping") {
+        REQUIRE(s.getCompartmentID(col1) == "");
+        REQUIRE(s.getCompartmentID(col2) == "compartment1");
+        REQUIRE(s.getCompartmentID(col3) == "compartment0");
+        REQUIRE(s.getCompartmentColour("compartment0") == col3);
+        REQUIRE(s.getCompartmentColour("compartment1") == col2);
+      }
+    }
+    WHEN("existing colour re-assigned") {
+      s.setCompartmentColour("compartment0", col1);
+      s.setCompartmentColour("compartment1", col2);
+      s.setCompartmentColour("compartment0", col2);
+      THEN("unassign old colour mapping") {
+        REQUIRE(s.getCompartmentID(col1) == "");
+        REQUIRE(s.getCompartmentID(col2) == "compartment0");
+        REQUIRE(s.getCompartmentColour("compartment0") == col2);
+        REQUIRE(s.getCompartmentColour("compartment1") == 0);
       }
     }
   }
