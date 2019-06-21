aa723388b6185fd3378cc951cc29baf65ceb7a74
Liam Keegan <liam@keegan.ch>
2019-06-21 17:43:28 +0200

    add testlib and QSignalSpy.h to tests

--- test/test_qlabelmousetracker.cpp
+++ test/test_qlabelmousetracker.cpp
@@ -1,7 +1,17 @@
+#include <QSignalSpy>
+
 #include "catch.hpp"
 #include "catch_qt_ostream.hpp"
-//#include <QSignalSpy>
 
-//#include "qlabelmousetracker.h"
+#include "qlabelmousetracker.h"
 
-TEST_CASE("qlabelmousetracker: todo") { REQUIRE(1 == 1); }
+TEST_CASE("qlabelmousetracker: todo", "[qlabelmousetracker]") {
+  QLabelMouseTracker mouseTracker;
+  QImage img(1, 1, QImage::Format_RGB32);
+  QRgb col = QColor(12, 243, 154).rgba();
+  img.setPixel(0, 0, col);
+  mouseTracker.setImage(img);
+  mouseTracker.show();
+  REQUIRE(mouseTracker.getImage().size() == img.size());
+  REQUIRE(mouseTracker.getImage().pixel(0, 0) == img.pixel(0, 0));
+}
