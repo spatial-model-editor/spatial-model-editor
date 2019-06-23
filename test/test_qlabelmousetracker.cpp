4d155aa0c37b075f6c9e5122522156fbd684ed1a
lkeegan <liam@keegan.ch>
2019-06-23 15:45:36 +0200

    add simple mouse click event to qLabelMousetracker test

--- test/test_qlabelmousetracker.cpp
+++ test/test_qlabelmousetracker.cpp
@@ -1,17 +1,37 @@
-#include <QSignalSpy>
+#include <QtTest/QtTest>
 
 #include "catch.hpp"
 #include "catch_qt_ostream.hpp"
 
 #include "qlabelmousetracker.h"
 
-TEST_CASE("qlabelmousetracker: todo", "[qlabelmousetracker]") {
+TEST_CASE("qlabelmousetracker", "[qlabelmousetracker]") {
   QLabelMouseTracker mouseTracker;
   QImage img(1, 1, QImage::Format_RGB32);
   QRgb col = QColor(12, 243, 154).rgba();
   img.setPixel(0, 0, col);
   mouseTracker.setImage(img);
+  mouseTracker.resize(10, 10);
   mouseTracker.show();
   REQUIRE(mouseTracker.getImage().size() == img.size());
   REQUIRE(mouseTracker.getImage().pixel(0, 0) == img.pixel(0, 0));
+  REQUIRE(mouseTracker.getColour() == col);
+
+  std::vector<QRgb> clicks;
+  QObject::connect(&mouseTracker, &QLabelMouseTracker::mouseClicked,
+                   [&](QRgb c) { clicks.push_back(c); });
+
+  REQUIRE(clicks.size() == 0);
+  // click on image:
+  QTest::mouseClick(&mouseTracker, Qt::LeftButton, Qt::KeyboardModifiers(),
+                    QPoint(mouseTracker.pos()) + QPoint(1, 1));
+  REQUIRE(clicks.size() == 1);
+  REQUIRE(clicks.back() == col);
+  REQUIRE(mouseTracker.getColour() == col);
+  // click again on image:
+  QTest::mouseClick(&mouseTracker, Qt::LeftButton, Qt::KeyboardModifiers(),
+                    QPoint(mouseTracker.pos() + QPoint(4, 3)));
+  REQUIRE(clicks.size() == 2);
+  REQUIRE(clicks.back() == col);
+  REQUIRE(mouseTracker.getColour() == col);
 }
