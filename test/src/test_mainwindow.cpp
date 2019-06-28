2891a633b35a942c448d601b6fc9a37f77f58570
Liam Keegan <liam@keegan.ch>
2019-06-28 16:05:57 +0200

    add test for GUI: user keypress ctrl+tab displays the next tab

--- test/src/test_mainwindow.cpp
+++ test/src/test_mainwindow.cpp
@@ -6,27 +6,45 @@
 
 constexpr int delay = 250;
 
-TEST_CASE("F1: opens about dialog", "[mainwindow][gui]") {
+constexpr int key_delay = 50;
+
+SCENARIO("Shortcut keys", "[gui][mainwindow]") {
   MainWindow w;
   w.show();
   // wait for mainwindow to load fully
   QTest::qWait(delay);
-  // close the next modal window to open
-  // after capturing the text in mwc.result
-  ModalWidgetCloser mwc;
-  // press F1: opens modal 'About' message box
-  // this message box is blocking until user clicks ok
-  // (or until the ModalWindowCloser closes it)
-  QTest::keyClick(&w, Qt::Key_F1);
-  QString correctText = "Spatial Model Editor";
-  REQUIRE(mwc.result.left(correctText.size()) == correctText);
-}
-
-TEST_CASE("ctrl+o: opens open file dialog", "[mainwindow][gui]") {
-  MainWindow w;
-  w.show();
-  QTest::qWait(delay);
-  ModalWidgetCloser mwc;
-  QTest::keyClick(&w, Qt::Key_O, Qt::ControlModifier);
-  REQUIRE(mwc.result == "QFileDialog::AcceptOpen");
+  WHEN("user presses F1") {
+    THEN("open About dialog box") {
+      // this object closes the next modal window to open
+      // after capturing the text in mwc.result
+      ModalWidgetCloser mwc;
+      // press F1: opens modal 'About' message box
+      // this message box is blocking until user clicks ok
+      // (or until the ModalWindowCloser closes it)
+      QTest::keyClick(&w, Qt::Key_F1);
+      QString correctText = "Spatial Model Editor";
+      REQUIRE(mwc.result.left(correctText.size()) == correctText);
+    }
+  }
+  WHEN("user presses ctrl+o") {
+    THEN("open FileDialog") {
+      ModalWidgetCloser mwc;
+      QTest::keyClick(&w, Qt::Key_O, Qt::ControlModifier);
+      REQUIRE(mwc.result == "QFileDialog::AcceptOpen");
+    }
+  }
+  WHEN("user presses ctrl+tab") {
+    THEN("display next tab") {
+      int nTabs = 7;
+      QTabWidget *tabMain =
+          w.topLevelWidget()->findChild<QTabWidget *>("tabMain");
+      REQUIRE(tabMain != nullptr);
+      REQUIRE(tabMain->currentIndex() == 0);
+      for (int index = 1; index < 2 * nTabs + 1; ++index) {
+        QTest::keyPress(w.windowHandle(), Qt::Key_Tab, Qt::ControlModifier,
+                        key_delay);
+        REQUIRE(tabMain->currentIndex() == index % nTabs);
+      }
+    }
+  }
 }
