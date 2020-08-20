03d850ad5f5385e0ad12ae39c5e68cb2e1c60313
Liam Keegan <liam@keegan.ch>
2020-08-20 14:37:59 +0200

    add manylinux1 python wheel builds to CI

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ class CMakeBuild(build_ext):
         build_args = ["--config", cfg]
 
         cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
-        build_args += ["--", "-j1"]
+        build_args += ["--", "-j2"]
 
         env = os.environ.copy()
         env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
