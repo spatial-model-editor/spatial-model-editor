2d64e4a26843f2ac6bcc44cfefa2335842546592
Liam Keegan <liam@keegan.ch>
2020-10-02 16:36:47 +0200

    v1.0.0 release

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ with open(path.join(sme_directory, "README.md")) as f:
 
 setup(
     name="sme",
-    version="0.9.3",
+    version="1.0.0",
     author="Liam Keegan",
     author_email="liam@keegan.ch",
     description="Spatial Model Editor python bindings",
