0df0b06f4b53a3aedde78a31a9eef5c56561a712
Liam Keegan <liam@keegan.ch>
2021-01-14 14:25:59 +0100

    refactor benchmarks
    - add bench.hpp with common datasets used for benchmarks
    - each dataset is a custom type (which contains a geometry image, colours, etc)
    - SME_BENCHMARK(f) macro constructs a templated benchmark of `f` for each dataset

--- src/core/mesh/src/interior_point_bench.cpp
+++ src/core/mesh/src/interior_point_bench.cpp
@@ -1,32 +1,12 @@
 #include "interior_point.hpp"
-#include <QImage>
-#include <QPointF>
-#include <QRgb>
-#include <benchmark/benchmark.h>
+#include "bench.hpp"
 
-static void
-interior_points_concave_cell_nucleus_100x100(benchmark::State &state) {
-  QImage img(":/geometry/concave-cell-nucleus-100x100.png");
-  QRgb col0 = img.pixel(0, 0);
-  QRgb col1 = img.pixel(35, 20);
-  QRgb col2 = img.pixel(40, 50);
+template <typename T> static void mesh_getInteriorPoints(benchmark::State &state) {
+  T data;
   std::vector<std::vector<QPointF>> interiorPoints;
   for (auto _ : state) {
-    interiorPoints = mesh::getInteriorPoints(img, {col0, col1, col2});
+    interiorPoints = mesh::getInteriorPoints(data.img, data.colours);
   }
 }
 
-static void
-interior_points_boundary_liver_cells_200x100(benchmark::State &state) {
-  QImage img(":/geometry/liver-cells-200x100.png");
-  QRgb col0 = img.pixel(50, 50);
-  QRgb col1 = img.pixel(40, 20);
-  QRgb col2 = img.pixel(21, 14);
-  std::vector<std::vector<QPointF>> interiorPoints;
-  for (auto _ : state) {
-    interiorPoints = mesh::getInteriorPoints(img, {col0, col1, col2});
-  }
-}
-
-BENCHMARK(interior_points_concave_cell_nucleus_100x100)->Unit(benchmark::kMillisecond);
-BENCHMARK(interior_points_boundary_liver_cells_200x100)->Unit(benchmark::kMillisecond);
+SME_BENCHMARK(mesh_getInteriorPoints);
--- src/core/model/src/geometry_bench.cpp
+++ src/core/model/src/geometry_bench.cpp
@@ -1,29 +1,20 @@
+#include "bench.hpp"
 #include "geometry.hpp"
-#include <benchmark/benchmark.h>
 
-static void compartment_concave_cell_nucleus_100x100(benchmark::State &state) {
-  QImage img(":/geometry/concave-cell-nucleus-100x100.png");
-  QRgb col0{img.pixel(0, 0)};
+template <typename T>
+static void geometry_Compartment(benchmark::State &state) {
+  T data;
   geometry::Compartment compartment;
   for (auto _ : state) {
-      compartment = geometry::Compartment("id", img, col0);
-    }
-  }
-
-static void compartment_liver_cells_200x100(benchmark::State &state) {
-  QImage img(":/geometry/liver-cells-200x100.png");
-  QRgb col0 {img.pixel(50, 50)};
-  geometry::Compartment compartment;
-  for (auto _ : state) {
-      compartment = geometry::Compartment("id", img, col0);
+    compartment = geometry::Compartment("id", data.img, data.colours[0]);
   }
 }
 
-static void concentration_image_array_concave_cell_nucleus_100x100(benchmark::State &state) {
-  QImage img(":/geometry/concave-cell-nucleus-100x100.png");
-  QRgb col0{img.pixel(0, 0)};
-  geometry::Compartment compartment("id", img, col0);
-  geometry::Field field(&compartment, "id", 1.0, col0);
+template <typename T>
+static void geometry_Field_getConcentrationImageArray(benchmark::State &state) {
+  T data;
+  geometry::Compartment compartment("id", data.img, data.colours[0]);
+  geometry::Field field(&compartment, "id", 1.0, data.colours[0]);
   field.setUniformConcentration(1.2);
   std::vector<double> concentration;
   for (auto _ : state) {
@@ -31,19 +22,5 @@ static void concentration_image_array_concave_cell_nucleus_100x100(benchmark::St
   }
 }
 
-static void concentration_image_array_liver_cells_200x100(benchmark::State &state) {
-  QImage img(":/geometry/liver-cells-200x100.png");
-  QRgb col0 {img.pixel(50, 50)};
-  geometry::Compartment compartment("id", img, col0);
-  geometry::Field field(&compartment, "id", 1.0, col0);
-  field.setUniformConcentration(1.2);
-  std::vector<double> concentration;
-  for (auto _ : state) {
-    concentration = field.getConcentrationImageArray();
-  }
-}
-
-BENCHMARK(compartment_concave_cell_nucleus_100x100)->Unit(benchmark::kMillisecond);
-BENCHMARK(compartment_liver_cells_200x100)->Unit(benchmark::kMillisecond);
-BENCHMARK(concentration_image_array_concave_cell_nucleus_100x100)->Unit(benchmark::kMillisecond);
-BENCHMARK(concentration_image_array_liver_cells_200x100)->Unit(benchmark::kMillisecond);
+SME_BENCHMARK(geometry_Compartment);
+SME_BENCHMARK(geometry_Field_getConcentrationImageArray);
