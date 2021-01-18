620f4206e594eec7774933efbf6d5cd9a8c623f2
Liam Keegan <liam@keegan.ch>
2021-01-18 13:10:47 +0100

    CMake improvements (#417)
    - allow build with BUILD_BENCHMARKS=off
    - allow build with BUILD_TESTING=off

--- benchmark/bench.cpp
+++ benchmark/bench.cpp
@@ -2,19 +2,13 @@
 #include "bench.hpp"
 #include <QtGlobal>
 #include <benchmark/benchmark.h>
-#include "dune_headers.hpp"
 
 int main(int argc, char **argv) {
   // load resources for sample images, models etc
   Q_INIT_RESOURCE(resources);
-  Q_INIT_RESOURCE(test_resources);
   // disable logging output
   spdlog::set_level(spdlog::level::off);
-  // init and mute DUNE logging
-  Dune::Logging::Logging::init(
-      Dune::FakeMPIHelper::getCollectiveCommunication());
-  Dune::Logging::Logging::mute();
-  // BENCHMARK_MAIN() from benchmark/benchmark.h without int main():
+  // contents of BENCHMARK_MAIN() copied from benchmark/benchmark.h:
   ::benchmark::Initialize(&argc, argv);
   if (::benchmark::ReportUnrecognizedArguments(argc, argv))
     return 1;
