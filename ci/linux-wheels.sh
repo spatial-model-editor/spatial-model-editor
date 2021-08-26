ee858360e9183bde340ba9a5120e40884b894614
Liam Keegan <liam@keegan.ch>
2021-08-26 20:32:29 +0200

    improve ccache use in wheel builds
    - cache ccache dir
    - note: linux docker image mounts root filesystem as /host

--- ci/linux-wheels.sh
+++ ci/linux-wheels.sh
@@ -4,6 +4,11 @@
 
 set -e -x
 
+ls /host
+export CCACHE_DIR=/host/home/runner/work/spatial-model-editor/spatial-model-editor/ccache
+
+ccache -s
+
 mkdir /project/build
 cd /project/build
 cmake .. \
