46f4518b2486f4af28dc2f0cd7f62bc7fed00bc9
Liam Keegan <liam@keegan.ch>
2021-11-22 13:55:40 +0100

    stick to ccache 4.3 to avoid libhiredis dependency

--- ci/linux-install.sh
+++ ci/linux-install.sh
@@ -7,7 +7,7 @@ set -e -x
 sudo apt-get update -yy
 
 # install build/test dependencies
-sudo apt-get install -yy ccache xvfb jwm lcov libhiredis
+sudo apt-get install -yy ccache xvfb jwm lcov
 
 # install qt build dependencies
 sudo apt-get install -yy libglu1-mesa-dev libx11-dev libx11-xcb-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libxcb-glx0-dev libxcb-keysyms1-dev libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev libxkbcommon-dev libxkbcommon-x11-dev '^libxcb.*-dev'
@@ -18,8 +18,9 @@ sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 100
 sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-9 100
 
 # get a recent version of ccache from conda-forge
-CCACHE_FILE="ccache-4.5-haef5404_0.tar.bz2"
-wget https://anaconda.org/conda-forge/ccache/4.5/download/linux-64/${CCACHE_FILE}
+# (note pinning to 4.3 as more recent versions depend on libhiredis >= 1)
+CCACHE_FILE="ccache-4.3-haef5404_1.tar.bz2"
+wget https://anaconda.org/conda-forge/ccache/4.3/download/linux-64/${CCACHE_FILE}
 tar -xf ${CCACHE_FILE}
 sudo cp bin/ccache /usr/bin/ccache
 
