67b60b766ea391b8b5ed37ce63681a65071a7a3e
Liam Keegan <liam@keegan.ch>
2021-11-22 15:13:27 +0100

    add LD_PRELOAD for asan job

--- ci/asan.sh
+++ ci/asan.sh
@@ -51,7 +51,7 @@ ccache --show-stats
 jwm &
 
 # run c++ tests
-time ./test/tests -as ~[expensive] > tests.txt 2>&1 || (tail -n 1000 tests.txt && exit 1)
+LD_PRELOAD=$(gcc -print-file-name=libasan.so) time ./test/tests -as ~[expensive] > tests.txt 2>&1 || (tail -n 1000 tests.txt && exit 1)
 tail -n 100 tests.txt
 
 # todo: also run with python lib
