23fecc7a8421f67d7f4705d44e464aa819d9e823
Liam Keegan <liam@keegan.ch>
2022-05-25 13:35:54 +0200

    implement multithreaded optimization
    - pagmo population -> archipelago
    - refactor pagmo UDP to be cheap to copy
      - now only contains a few pointers, no other work done in constructor
      - pop a model to simulate from a threadsafe queue when needed
        - fallback is to create a model if the queue is empty
      - when done simulating, push model back to the queue
      - resolves #784
    - add `OptAlgorithm` struct to `OptimizeOptions` with
      - `OptAlgorithm`: enum class, currently
        - PSO
        - GPSO
      - `islands`: number of islands (typically ~ number of CPUs)
      - `population`: population on each island
    - add islands spin box to DialogOptSetup

--- core/simulate/src/optimize_impl.hpp
+++ core/simulate/src/optimize_impl.hpp
@@ -9,7 +9,6 @@
 namespace sme::simulate {
 
 void applyParameters(const pagmo::vector_double &values,
-                     const std::vector<OptParam> &optParams,
                      sme::model::Model *model);
 
 double calculateCosts(const std::vector<OptCost> &optCosts,
@@ -19,38 +18,24 @@ double calculateCosts(const std::vector<OptCost> &optCosts,
 /**
  * @brief Implements a Pagmo User Defined Problem to evolve
  *
- * @note Needs to be copy-constructible, implement fitness() and get_bounds()
- * functions and be thread-safe, see
+ * @note Needs to be (cheaply) copy-constructible, implement `fitness()` and
+ * `get_bounds()` functions, and be thread-safe, see
  * https://esa.github.io/pagmo2/docs/cpp/problem.html
- *
- * @note Pagmo also assumes a UDP is cheap to copy, in particular when using
- * Archipelagos for parallel evolution many copies are made. Currently ours is
- * not cheap to copy as it also makes a copy of the model (in order to be thread
- * safe when modifying/simulating this model).
  */
-
-struct OptTimestep {
-  // the time to simulate for
-  double simulationTime;
-  // the indices of the OptCosts to calculate after simulating
-  std::vector<std::size_t> optCostIndices;
-};
 
 class PagmoUDP {
 private:
-  std::string xmlModel;
-  std::unique_ptr<sme::model::Model> model;
-  OptimizeOptions optimizeOptions;
-  std::vector<OptTimestep> optTimesteps;
+  ThreadsafeModelQueue *modelQueue{nullptr};
+  const std::string *xmlModel{nullptr};
+  const OptimizeOptions *optimizeOptions{nullptr};
+  const std::vector<OptTimestep> *optTimesteps{nullptr};
 
 public:
-  explicit PagmoUDP();
-  PagmoUDP(PagmoUDP &&) noexcept;
-  PagmoUDP &operator=(PagmoUDP &&) noexcept;
-  PagmoUDP &operator=(const PagmoUDP &other);
-  PagmoUDP(const PagmoUDP &other);
-  ~PagmoUDP();
-  void init(const std::string &xml, const OptimizeOptions &options);
+  PagmoUDP() = default;
+  explicit PagmoUDP(const std::string *xmlModel,
+                    const OptimizeOptions *optimizeOptions,
+                    const std::vector<OptTimestep> *optTimesteps,
+                    ThreadsafeModelQueue *modelQueue);
   [[nodiscard]] pagmo::vector_double
   fitness(const pagmo::vector_double &dv) const;
   [[nodiscard]] std::pair<pagmo::vector_double, pagmo::vector_double>
