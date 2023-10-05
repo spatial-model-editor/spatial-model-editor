9fd91e12835f2c30b034202c9285b90c3d09605d
Horea Caramizaru <horea.caramizaru@iwr.uni-heidelberg.de>
2023-10-05 12:16:01 +0200

    switch to autmatic memory management

--- gui/rendering/Utils.cpp
+++ gui/rendering/Utils.cpp
@@ -165,33 +165,39 @@ std::string rendering::Utils::Backtrace(const std::string sectionName,
   const int nMaxFrames = sizeof(callstack) / sizeof(callstack[0]);
   char buf[1024];
   int nFrames = backtrace(callstack, nMaxFrames);
-  char **symbols = backtrace_symbols(callstack, nFrames);
-
+  typedef char* ptrChar;
+  std::shared_ptr<ptrChar[]> symbolsPtr =
+      std::shared_ptr<ptrChar[]>(
+          backtrace_symbols(callstack, nFrames),
+          [](char **pi) { free(pi);}
+          );
   std::ostringstream trace_buf;
   for (int i = skip; i < nFrames; i++) {
-    printf("%s\n", symbols[i]);
+    printf("%s\n", symbolsPtr[i]);
 
     Dl_info info;
     if (dladdr(callstack[i], &info) && info.dli_sname) {
-      char *demangled = nullptr;
+      std::shared_ptr<char[]> demangledPtr;
       int status = -1;
       if (info.dli_sname[0] == '_')
-        demangled =
-            abi::__cxa_demangle(info.dli_sname, nullptr, nullptr, &status);
+        demangledPtr = std::shared_ptr<char[]>(
+            abi::__cxa_demangle(
+                info.dli_sname,
+                nullptr,
+                nullptr, &status),
+            [](char *pi) { free(pi); });
       snprintf(buf, sizeof(buf), "%-3d %*p %s + %zd\n", i,
                int(2 + sizeof(void *) * 2), callstack[i],
-               status == 0                 ? demangled
-               : info.dli_sname == nullptr ? symbols[i]
+               status == 0                 ? demangledPtr.get()
+               : info.dli_sname == nullptr ? symbolsPtr[i]
                                            : info.dli_sname,
                (char *)callstack[i] - (char *)info.dli_saddr);
-      free(demangled);
     } else {
       snprintf(buf, sizeof(buf), "%-3d %*p %s\n", i,
-               int(2 + sizeof(void *) * 2), callstack[i], symbols[i]);
+               int(2 + sizeof(void *) * 2), callstack[i], symbolsPtr[i]);
     }
     trace_buf << buf;
   }
-  free(symbols);
   if (nFrames == nMaxFrames)
     trace_buf << "[truncated]\n";
   return sectionName + trace_buf.str();
