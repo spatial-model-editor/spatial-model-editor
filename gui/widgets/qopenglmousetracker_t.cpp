cbe2b58329e69018f4e786d71d7a3591ec88c67b
Horea Caramizaru <horea.caramizaru@iwr.uni-heidelberg.de>
2024-03-12 02:51:22 +0100

    add wait

--- gui/widgets/qopenglmousetracker_t.cpp
+++ gui/widgets/qopenglmousetracker_t.cpp
@@ -154,6 +154,7 @@ TEST_CASE("QOpenGLMouseTracker: OpenGL", tags) {
       wait(100);
 
       sendMouseClick(&test, {331, 322});
+      wait(100);
       QcolorSelection = QColor(test.getColour());
       REQUIRE(redColor == QcolorSelection);
 
