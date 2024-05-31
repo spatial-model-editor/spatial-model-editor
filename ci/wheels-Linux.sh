17778c8372a80bbcba509e39c94aa7e7aaf7f752
Liam Keegan <liam@keegan.ch>
2024-05-31 13:25:36 +0200

    Fix manylinux wheel caching on CI by specifying correct ccache dir

--- ci/wheels-Linux.sh
+++ ci/wheels-Linux.sh
@@ -4,7 +4,7 @@
 
 set -e -x
 
-export CIBW_ENVIRONMENT='CMAKE_ARGS="-DSME_BUILD_CORE=OFF -DCMAKE_PREFIX_PATH=/opt/smelibs;/opt/smelibs/lib64/cmake"'
+export CIBW_ENVIRONMENT='CMAKE_ARGS="-DSME_BUILD_CORE=OFF -DCMAKE_PREFIX_PATH=/opt/smelibs;/opt/smelibs/lib64/cmake" CCACHE_DIR=/host/home/runner/.cache/ccache'
 export CIBW_BEFORE_ALL='bash {project}/ci/wheels-manylinux.sh'
 
 python -m pip install cibuildwheel=="$CIBUILDWHEEL_VERSION"
