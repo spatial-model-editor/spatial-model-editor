a98a72b97afc0b7fdc322182951c94c61865137e
Liam Keegan <liam@keegan.ch>
2026-03-17 21:46:25 +0100

    Add concentration plot to image slice dialog (#1138)
    - add a plot of species concentrations under the sliced image
    - x axis is the distance along the slice
    - y axis is species concentration
    - add a slider to choose the time-slice for this plot
    - resolves #1134

--- gui/dialogs/dialogimageslice.hpp
+++ gui/dialogs/dialogimageslice.hpp
@@ -2,13 +2,29 @@
 
 #include "sme/image_stack.hpp"
 #include <QDialog>
+#include <QStringList>
 #include <memory>
+#include <vector>
 
 namespace Ui {
 class DialogImageSlice;
 }
 
+namespace sme::simulate {
+class Simulation;
+}
+
 enum class SliceType { Horizontal, Vertical, Custom };
+
+struct DialogImageSlicePlotData {
+  const sme::simulate::Simulation *simulation{nullptr};
+  QStringList compartmentNames{};
+  std::vector<std::vector<std::size_t>> speciesToDraw{};
+  QString timeUnit{};
+  QString lengthUnit{};
+  QString concentrationUnit{};
+  int timepointIndex{-1};
+};
 
 class DialogImageSlice : public QDialog {
   Q_OBJECT
@@ -17,6 +33,7 @@ public:
   explicit DialogImageSlice(const sme::common::ImageStack &geometryImage,
                             const QVector<sme::common::ImageStack> &images,
                             const QVector<double> &timepoints, bool invertYAxis,
+                            DialogImageSlicePlotData plotData = {},
                             QWidget *parent = nullptr);
   ~DialogImageSlice() override;
   QImage getSlicedImage() const;
@@ -27,13 +44,28 @@ private:
   const std::unique_ptr<Ui::DialogImageSlice> ui;
   const QVector<sme::common::ImageStack> &imgs;
   const QVector<double> &time;
+  const DialogImageSlicePlotData plotData;
   sme::common::ImageStack slice;
-  SliceType sliceType;
-  int horizontal;
-  int vertical;
+  QVector<double> distanceAlongSlice;
+  std::vector<std::size_t> sliceArrayIndices;
+  SliceType sliceType{SliceType::Vertical};
+  int horizontal{0};
+  int vertical{0};
+  int selectedTimepointIndex{0};
+  bool fixedAxisRangesValid{false};
+  double fixedXAxisMin{0.0};
+  double fixedXAxisMax{1.0};
+  double fixedYAxisMin{0.0};
+  double fixedYAxisMax{1.0};
   QPoint startPoint{0, 0};
   QPoint endPoint{0, 0};
+  void initConcentrationPlot();
+  void updateSlicePlotData();
+  void updateFixedAxisRanges();
   void updateSlicedImage();
+  void updateDisplayedSlicedImage();
+  void updateSelectedTimepointText();
+  void updateConcentrationPlot();
   void saveSlicedImage();
   void cmbSliceType_activated(int index);
   void lblSlice_mouseDown(QPoint point);
@@ -41,4 +73,5 @@ private:
   void lblSlice_mouseWheelEvent(int delta);
   void lblSlice_mouseOver(QPoint point);
   void lblImage_mouseOver(const sme::common::Voxel &voxel);
+  void slideTimepoint_valueChanged(int value);
 };
