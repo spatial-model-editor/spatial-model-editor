7746158679ca579197012a62984591e8ae74e12b
Harald Mack <harald.mack@protonmail.com>
2026-04-28 15:48:28 +0200

    add quantiles to feature selection

--- gui/tabs/tabfeatures.cpp
+++ gui/tabs/tabfeatures.cpp
@@ -71,6 +71,9 @@ TabFeatures::TabFeatures(sme::model::Model &m, QLabelMouseTracker *mouseTracker,
   ui->cmbReduction->addItem("Sum");
   ui->cmbReduction->addItem("Min");
   ui->cmbReduction->addItem("Max");
+  ui->cmbReduction->addItem("First quantile");
+  ui->cmbReduction->addItem("Median");
+  ui->cmbReduction->addItem("Third quantile");
 }
 
 TabFeatures::~TabFeatures() = default;
